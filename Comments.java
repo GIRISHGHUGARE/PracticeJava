ad537d5372be148f7518a09ba9253cc8f00e66d6
Girish Ghugare <girishghugare19@gmail.com>
2024-05-21 13:49:54 +0530

    Added sumofAB file and Comments file for java

---
+++ Comments.java
@@ -0,0 +1,10 @@
+public class Comments {
+    public static void main(String args[]){
+        //This is a single line comment
+        System.out.println("Welcome Girish Ghugare");
+        /*
+        This is a 
+        multi line comment
+        */
+    }
+}
