d51182944aec13e0ced9f213cca4c8ef0f432e39
Girish Ghugare <girishghugare19@gmail.com>
2024-05-21 13:27:05 +0530

    Variables File Added

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myIntMarks = 90;
+        float myFloatMarks = 90.1f;
+        char myDivision = 'D';
+        String myStringName = "Girish Ghugare";
+        boolean myBooleanValue = true;
+        System.out.println("My name is :- " + myStringName);
+        System.out.println("My division :- " + myDivision);
+        System.out.println("My marks in integer value :- " + myIntMarks);
+        System.out.println("My marks in integer value :- " + myFloatMarks);
+        System.out.println("Result status :- " + myBooleanValue);
+    }
+}
