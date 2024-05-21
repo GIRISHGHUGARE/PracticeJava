d913a4e55450bb907ad7d108537763280e6455df
Girish Ghugare <girishghugare19@gmail.com>
2024-05-21 13:39:11 +0530

    Data Types added

--- DataTypes.java
+++ DataTypes.java
@@ -1,10 +1,13 @@
-public class Variables {
+public class DataTypes {
     public static void main(String[] args) {
         int myIntMarks = 90;
         float myFloatMarks = 90.1f;
         char myDivision = 'D';
         String myStringName = "Girish Ghugare";
         boolean myBooleanValue = true;
+        byte myByteNum = 8;
+        short myShortNum = 12;
+        long myLongNum = 182731;
         System.out.println("My name is :- " + myStringName);
         System.out.println("My division :- " + myDivision);
         System.out.println("My marks in integer value :- " + myIntMarks);
