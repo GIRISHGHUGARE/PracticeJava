7a207687dc6a9c37c78ba7a1eae6a0a18f844ec3
Girish Ghugare <girishghugare19@gmail.com>
2024-05-21 11:55:20 +0530

    change in basic java file

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,6 +1,7 @@
 public class JavaBasics {
     public static void main(String args[]){
-        System.out.print("Hello World");
+        System.out.println("Hello Girish Ghugare");
+        System.out.println("You are welcome to the Java World");
     }
 }
 //boilerplate code
